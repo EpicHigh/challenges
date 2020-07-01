119d77e030abfa89f069f59e30a33abc8d9ab60c
EpicHigh <prasit_tongpradit@windowslive.com>
2020-07-01 07:36:30 +0700

    💄 Adding the UI and style files.

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './store';
+import GlobalStyles from './styles/global';
 
 render(
   <Provider store={store}>
+    <GlobalStyles />
     <App />
   </Provider>,
   document.getElementById('root'),
