55f6b32bfd1f4210d67fa196e9483cb9b44f8a55
EpicHigh <prasit_tongpradit@windowslive.com>
2020-07-01 08:49:44 +0700

    💄 Updating style of file

--- src/helpers/helpers.test.js
+++ src/helpers/helpers.test.js
@@ -2,7 +2,7 @@ import { describe, expect } from '@jest/globals';
 import { summaryDonations } from './helpers';
 
 describe('helpers', function () {
-  test('`summaryDonations` should calculate donations correctly', function () {
+  it('`summaryDonations` should calculate donations correctly', function () {
     expect(summaryDonations([1, 2, 3, 4])).toEqual(10);
   });
 });
