221daa76c3143f5b48f91949e48b32bf44cc6e7e
EpicHigh <prasit_tongpradit@windowslive.com>
2020-07-01 07:39:01 +0700

    🎨 Improving structure

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-import { getCharities, getPayments, makePayments } from './store/actions';
-import { getCharityList, getMessageState, getDonateState } from './store/selectors';
-import { payments } from './static';
-
-const Card = styled.div`
-  border: 1px solid #ccc;
-  margin: 10px;
-`;
-
-const Box = styled.div`
-  display: flex;
-`;
-
-const Success = styled.p`
-  color: green;
-  font-size: 16px;
-  font-weight: bold;
-  margin: 1em 0;
-  text-align: center;
-`;
-
-const Error = styled.p`
-  color: red;
-  font-size: 16px;
-  font-weight: bold;
-  margin: 1em 0;
-  text-align: center;
-`;
+import { getCharities, getPayments } from './store/actions';
+import { getMessageState, getDonateState } from './store/selectors';
+import Error from './components/common/Error';
+import Success from './components/common/Success';
+import Header from './components/common/Header';
+import Container from './components/common/Container';
+import Donation from './components/common/Donation';
+import Box from './components/common/Box';
+import { HomePage } from './components/pages/HomePage';
 
 const App = () => {
-  const [selectedAmount, setSelectedAmount] = useState(10);
   const dispatch = useDispatch();
 
-  const charities = useSelector(getCharityList);
   const message = useSelector(getMessageState);
   const donate = useSelector(getDonateState);
 
@@ -43,39 +21,16 @@ const App = () => {
     dispatch(getPayments());
   }, [dispatch]);
 
-  const handlePayments = useCallback(
-    (charitiesId, amount, currency) => () => dispatch(makePayments(charitiesId, amount, currency)),
-    [dispatch],
-  );
-
   return (
-    <div>
-      <h1>Tamboon React</h1>
-      <p>All donations: {donate}</p>
-      {message.success && <Success>{message.success}</Success>}
-      {message.error && <Error>{message.error}</Error>}
-      {charities?.map((charity, i) => (
-        <Card key={`${i}`}>
-          <p>{charity?.name}</p>
-          <Box>
-            {payments?.map((amount, j) => (
-              <div key={`payment-${j}`}>
-                <input
-                  id={`${amount}-payment-${i}${j}`}
-                  name="payment"
-                  type="radio"
-                  onClick={() => setSelectedAmount(amount)}
-                />
-                <label htmlFor={`${amount}-payment-${i}${j}`}>{amount}</label>
-              </div>
-            ))}
-            <button type="submit" onClick={handlePayments(charity.id, selectedAmount, charity.currency)}>
-              Pay
-            </button>
-          </Box>
-        </Card>
-      ))}
-    </div>
+    <Container>
+      <Box top={4}>
+        <Header>Omise Tamboon React</Header>
+        <Donation>All donations: {donate}</Donation>
+        {message.success && <Success>{message.success}</Success>}
+        {message.error && <Error>{message.error}</Error>}
+        <HomePage />
+      </Box>
+    </Container>
   );
 };
 
